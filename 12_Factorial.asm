bc619da2f5970adc789781c0a84b5957a2e73ff1
Akshat Raj <ak2084788@gmail.com>
2026-07-18 07:33:47 +0530

    Add assembly code for calculating factorial of a number

---
+++ 12_Factorial.asm
@@ -0,0 +1,23 @@
+
+;<Program title>
+
+lda 0005h
+mov d,a
+mov b,a
+dcr b
+
+loop: mov h,b
+
+mvi a,0
+loop1: add d
+       dcr h
+       jnz loop1
+
+       mov d,a
+
+       dcr b
+       jnz loop
+sta 0000h
+
+
+hlt
